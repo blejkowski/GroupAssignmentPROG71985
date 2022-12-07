1761a5a12a6730de99a2a6420051e4bb9d569ef9
Aidan <anickerson8778@conestogac.on.ca>
2022-12-06 22:33:17 -0500

    added bryans Contribution to node.h

--- GroupAssignmentPROG71985/node.h
+++ GroupAssignmentPROG71985/node.h
@@ -1,3 +1,10 @@
+/*PROG1985 F22 Section2 - Group Project - node.h
+*Author: Bryan Lejkowski
+*Main Contributor: Bryan Lejkowski
+*Contributed to by:
+*
+* This module contains the declarations/prototypes of the operations functions to be called by the switch in switch.c
+*/
 #pragma once
 #include "task.h"
 typedef struct listnode {
